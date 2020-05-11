b1fa7785d470f72ba125c9c4377118f457f20157
Sami <samialeksi.tuomela@gmail.com>
2020-05-12 01:49:31 +0300

    lisätty buildi

---
+++ build/precache-manifest.1e066abc285188ebecc28433d25dd82f.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "619f59533b003dca1b345be859c75821",
+    "url": "/index.html"
+  },
+  {
+    "revision": "72972b2d9a53c6b88cad",
+    "url": "/static/css/main.9d414ebc.chunk.css"
+  },
+  {
+    "revision": "c07a4f030c1cb7a74dbe",
+    "url": "/static/js/2.3b99e57e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3b99e57e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "72972b2d9a53c6b88cad",
+    "url": "/static/js/main.1d346a2f.chunk.js"
+  },
+  {
+    "revision": "11ec17efefa50eef38fe",
+    "url": "/static/js/runtime-main.558a4289.js"
+  }
+]);
---
+++ build/precache-manifest.1f3430d77caf6ea28c3e3d7979da19c1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3aacf6297520691ac7a714891cd21f5b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "1a01c876ba410499c9b8",
+    "url": "/static/css/main.9d414ebc.chunk.css"
+  },
+  {
+    "revision": "c07a4f030c1cb7a74dbe",
+    "url": "/static/js/2.3b99e57e.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.3b99e57e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1a01c876ba410499c9b8",
+    "url": "/static/js/main.c6ce2341.chunk.js"
+  },
+  {
+    "revision": "11ec17efefa50eef38fe",
+    "url": "/static/js/runtime-main.558a4289.js"
+  }
+]);
